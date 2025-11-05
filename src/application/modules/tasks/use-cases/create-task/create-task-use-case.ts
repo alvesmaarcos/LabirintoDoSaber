e4b7b42f45556cbf7087d407db93b00716995707
Natan Lucena <zerogamerptbr@gmail.com>
2025-11-04 22:57:13 -0300

    :recycle: refac: fixes

--- src/application/modules/tasks/use-cases/create-task/create-task-use-case.ts
+++ src/application/modules/tasks/use-cases/create-task/create-task-use-case.ts
@@ -1,59 +1,62 @@
 import { failure, success } from "@wave-telecom/framework/core";
 import { TaskRepository } from "../../../../../domain/repositories/task-repository";
-import { Task, TaskCategory, TaskType } from "../../../../../domain/entities/task";
+import {
+  Task,
+  TaskCategory,
+  TaskType,
+} from "../../../../../domain/entities/task";
 
 interface CreateTaskUseCaseRequest {
-    category: TaskCategory;
-    type: TaskType;
-    prompt: string;
-    alternatives: {
-        text: string;
-        isCorrect: boolean;
-    }[];
-    imageFile?: string;
-    audioFile?: string;
+  category: TaskCategory;
+  type: TaskType;
+  prompt: string;
+  alternatives: {
+    text: string;
+    isCorrect: boolean;
+  }[];
+  imageFile?: string;
+  audioFile?: string;
 }
 
 export class CreateTaskUseCase {
-    constructor(private taskRepository: TaskRepository) {}
+  constructor(private taskRepository: TaskRepository) {}
 
-    async execute(props: CreateTaskUseCaseRequest) {
-        if (!Object.values(TaskType).includes(props.type)) {
-            return failure("INVALID_TASK_TYPE");
-        }
+  async execute(props: CreateTaskUseCaseRequest) {
+    if (!Object.values(TaskType).includes(props.type)) {
+      return failure("INVALID_TASK_TYPE");
+    }
 
-        if (props.type === TaskType.MultipleChoice) {
-            if (props.imageFile || props.audioFile) {
-                return failure("TEXT_TASK_CANNOT_HAVE_MEDIA");
-            }
-        }
+    if (
+      props.type === TaskType.MultipleChoice &&
+      (props.imageFile || props.audioFile)
+    ) {
+      return failure("TEXT_TASK_CANNOT_HAVE_MEDIA");
+    }
+    if (props.type === TaskType.MultipleChoiceWithMedia) {
+      const hasImage = !!props.imageFile;
+      const hasAudio = !!props.audioFile;
+      if (!hasImage && !hasAudio) {
+        return failure("MEDIA_TASK_REQUIRES_IMAGE_OR_AUDIO");
+      }
+    }
 
-        if (props.type === TaskType.MultipleChoiceWithMedia) {
-            const hasImage = !!props.imageFile;
-            const hasAudio = !!props.audioFile;
-            if (!hasImage && !hasAudio) {
-                return failure("MEDIA_TASK_REQUIRES_IMAGE_OR_AUDIO");
-            }
-        }
+    const hasCorrectAlternative = props.alternatives.some(
+      (alt) => alt.isCorrect === true
+    );
 
-        const hasCorrectAlternative = props.alternatives.some(
-            (alt) => alt.isCorrect === true
-        );
+    if (!hasCorrectAlternative) {
+      return failure("AT_LEAST_ONE_ALTERNATIVE_MUST_BE_CORRECT");
+    }
 
-        if (!hasCorrectAlternative) {
-            return failure("AT_LEAST_ONE_ALTERNATIVE_MUST_BE_CORRECT");
-        }
-        
-        const taskResult = Task.create(props);
-        if (!taskResult.ok) {
-            return failure("INVALID_TASK_DATA");
-        }
-        
-        const task = taskResult.value;
+    const taskResult = Task.create(props);
+    if (!taskResult.ok) {
+      return failure("INVALID_TASK_DATA");
+    }
 
-        await this.taskRepository.save(task);
+    const task = taskResult.value;
 
-        return success(void 0);
-    }       
+    await this.taskRepository.save(task);
+
+    return success(void 0);
+  }
 }
-
