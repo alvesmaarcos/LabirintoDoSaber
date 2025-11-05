1086d30a0848dad9da80ff4a3f82a74982834889
Natan Lucena <zerogamerptbr@gmail.com>
2025-11-04 23:02:51 -0300

    :lock: security: add middleware

--- src/application/modules/tasks/routes/index.ts
+++ src/application/modules/tasks/routes/index.ts
@@ -4,6 +4,8 @@ import { CreateTaskUseCase } from "../use-cases/create-task/create-task-use-case
 import { CreateTaskController } from "../use-cases/create-task/create-task-controller";
 import { ListTasksUseCase } from "../use-cases/list-tasks/list-tasks-use-case";
 import { ListTasksController } from "../use-cases/list-tasks/list-tasks-controller";
+import { MockEducatorRepository } from "../../../../infraestructure/repositories/mock/educator-repository-impl";
+import { makeAuthMiddleware } from "../../../../infraestructure/middlewares";
 
 const tasksRouter = Router();
 
@@ -12,6 +14,12 @@ const taskRepository = new MockTaskRepository();
 const createTaskUseCase = new CreateTaskUseCase(taskRepository);
 
 const listTasksUseCase = new ListTasksUseCase(taskRepository);
+
+const educatorRepository = new MockEducatorRepository();
+
+const authMiddleware = makeAuthMiddleware(educatorRepository);
+
+tasksRouter.use(authMiddleware);
 
 tasksRouter.post("/create", (req: Request, res: Response) => {
   new CreateTaskController(createTaskUseCase).execute(req, res);
