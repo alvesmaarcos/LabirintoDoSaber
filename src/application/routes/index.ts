7d6a67e27392d2a2aa4fc31e0373d47be07e8c6e
harielfr <enzoharielferreira@gmail.com>
2025-11-12 21:33:01 -0300

    style: coreção swagger e rotas

--- src/application/routes/index.ts
+++ src/application/routes/index.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { educatorRouter } from "../modules/educator/routes";
 import { studentRouter } from "../modules/student/routes";
-import { tasksRouter } from "../modules/tasks/routes";
+import { taskRouter } from "../modules/task/routes";
 
 const router = Router();
 
-router.use("/educators", educatorRouter);
-router.use("/students", studentRouter);
-router.use("/tasks", tasksRouter);
+router.use("/educator", educatorRouter);
+router.use("/student", studentRouter);
+router.use("/task", taskRouter);
 
 export { router };
