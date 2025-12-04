80aee7714878571944913af426a61a30bb4da326
Natan Lucena <zerogamerptbr@gmail.com>
2025-12-04 03:08:23 -0300

    :sparkles: feat: add name in session

--- src/application/modules/task-notebook-session/use-cases/generator-report-task-notebook-session/generator-report-task-notebook-session-use-case.ts
+++ src/application/modules/task-notebook-session/use-cases/generator-report-task-notebook-session/generator-report-task-notebook-session-use-case.ts
@@ -29,74 +29,91 @@ export class GeneratorReportTaskNotebookSessionUseCase {
       tasksById[answer.taskId.value] = task;
     }
 
-    const totalTimeSession = (session.finishedAt?.getTime()! - session.startedAt.getTime()) / 1000; // segundos
+    const totalTimeSession =
+      (session.finishedAt?.getTime()! - session.startedAt.getTime()) / 1000; // segundos
 
     const totalQuestions = session.answers.length;
 
-    const averageTimePerQuestion = session.answers.reduce((acc, curr) => acc + curr.timeToAnswer, 0) / totalQuestions; // segundos
+    const averageTimePerQuestion =
+      session.answers.reduce((acc, curr) => acc + curr.timeToAnswer, 0) /
+      totalQuestions; // segundos
 
-    const correctTimes = session.answers.filter((a) => a.isCorrect).map((a) => a.timeToAnswer);
+    const correctTimes = session.answers
+      .filter((a) => a.isCorrect)
+      .map((a) => a.timeToAnswer);
 
-    const incorrectTimes = session.answers.filter((a) => !a.isCorrect).map((a) => a.timeToAnswer);
+    const incorrectTimes = session.answers
+      .filter((a) => !a.isCorrect)
+      .map((a) => a.timeToAnswer);
 
-    const averageCorrectTime = correctTimes.length > 0 
-    ? correctTimes.reduce((acc, n) => acc + n, 0) / correctTimes.length 
-    : null; // segundos
+    const averageCorrectTime =
+      correctTimes.length > 0
+        ? correctTimes.reduce((acc, n) => acc + n, 0) / correctTimes.length
+        : null; // segundos
 
-    const averageIncorrectTime = incorrectTimes.length > 0
-    ? incorrectTimes.reduce((acc, n) => acc + n, 0) / incorrectTimes.length
-    : null; // segundos
+    const averageIncorrectTime =
+      incorrectTimes.length > 0
+        ? incorrectTimes.reduce((acc, n) => acc + n, 0) / incorrectTimes.length
+        : null; // segundos
 
-    const statsByCategory: Record<string, { total: number; correct: number; percentage: number | null } > = {};
+    const statsByCategory: Record<
+      string,
+      { total: number; correct: number; percentage: number | null }
+    > = {};
 
-    const statsByType: Record<string, { total: number; correct: number; percentage: number | null } > = {};
+    const statsByType: Record<
+      string,
+      { total: number; correct: number; percentage: number | null }
+    > = {};
 
     for (const answer of session.answers) {
-        const task = tasksById[answer.taskId.value];
-        if (!task) continue;
+      const task = tasksById[answer.taskId.value];
+      if (!task) continue;
 
-        const category = task.category;
-        const type = task.type;
+      const category = task.category;
+      const type = task.type;
 
-        if (!statsByCategory[category]) {
-            statsByCategory[category] = { total: 0, correct: 0, percentage: null };
-        }
-        statsByCategory[category].total++;
-        if (answer.isCorrect) {
-            statsByCategory[category].correct++;
-        }
+      if (!statsByCategory[category]) {
+        statsByCategory[category] = { total: 0, correct: 0, percentage: null };
+      }
+      statsByCategory[category].total++;
+      if (answer.isCorrect) {
+        statsByCategory[category].correct++;
+      }
 
-        if (!statsByType[type]) {
-            statsByType[type] = { total: 0, correct: 0, percentage: null };
-        }
-        statsByType[type].total++;
-        if (answer.isCorrect) {
-            statsByType[type].correct++;
-        }
+      if (!statsByType[type]) {
+        statsByType[type] = { total: 0, correct: 0, percentage: null };
+      }
+      statsByType[type].total++;
+      if (answer.isCorrect) {
+        statsByType[type].correct++;
+      }
     }
 
     const percentageByCategory: Record<string, number | null> = {};
 
     for (const category in statsByCategory) {
-        const { total, correct } = statsByCategory[category];
-        percentageByCategory[category] = total > 0 ? (correct / total) * 100 : null; // porcentagem
+      const { total, correct } = statsByCategory[category];
+      percentageByCategory[category] =
+        total > 0 ? (correct / total) * 100 : null; // porcentagem
     }
 
     const percentageByType: Record<string, number | null> = {};
 
     for (const type in statsByType) {
-        const { total, correct } = statsByType[type];
-        percentageByType[type] = total > 0 ? (correct / total) * 100 : null; // porcentagem
+      const { total, correct } = statsByType[type];
+      percentageByType[type] = total > 0 ? (correct / total) * 100 : null; // porcentagem
     }
 
     return success({
-    totalTimeSession,
-    totalQuestions,
-    averageTimePerQuestion,
-    averageCorrectTime,
-    averageIncorrectTime,
-    percentageByCategory,
-    percentageByType,
+      sessionName: session.name,
+      totalTimeSession,
+      totalQuestions,
+      averageTimePerQuestion,
+      averageCorrectTime,
+      averageIncorrectTime,
+      percentageByCategory,
+      percentageByType,
     });
   }
 }
